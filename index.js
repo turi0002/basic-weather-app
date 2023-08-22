63bf1c412873b326c5510dc32d5c8cde2262ae20
turi0002 <trui0002@gmail.com>
2023-08-22 16:03:39 +0300

    YOUR_EXPLANATION

---
+++ index.js
@@ -0,0 +1,14 @@
+// http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cc0606848d1ad036138ea63b1140dd2d
+
+const element = document.getElementById('myInput');
+const search = document.getElementById('search-box');
+myInput.addEventListener('keypress', function(event) {
+    if (event.key === "Enter") {
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cc0606848d1ad036138ea63b1140dd2d')
+       
+        
+       
+    }
+    
+  });
+
